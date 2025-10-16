14dcb66dca53eafa1534085c8fba614b50cec260
aytajgaribova <aytajyg-bpa203@code.edu.az>
2025-10-16 18:46:08 +0400

    task

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace ConsoleApp1;
+﻿
+using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
+        Console.Write("Cümləni daxil et: ");
+        string cumle = Console.ReadLine();
+
+        string[] sozler = cumle.Split(' ');
+        string enBoyukSoz = "";
+        int enBoyukUzunluq = 0;
+
+        for (int i = 0; i < sozler.Length; i++)
+        {
+            if (sozler[i].Length > enBoyukUzunluq)
+            {
+                enBoyukUzunluq = sozler[i].Length;
+                enBoyukSoz = sozler[i];
+            }
+        }
+
+        Console.WriteLine("Ən uzun söz:"+ enBoyukSoz);
+        Console.WriteLine("Uzunluğu:" + enBoyukUzunluq);
     }
 }
